8991b38375cd5aec51c57f084c95b1b7bffd03ed
TTeun <teunverstraaten@gmail.com>
2015-09-26 18:19:52 +0200

    Stuur nu alle count variabelen van elke core rond naar de anderen. zo kan het totaal aantal prime getallen worden gevonden. Ook kan deze info gebruikt worden als de laatste k priemgetallen moet worden geprint, dan kunnen we met de subcounters kijken in welke core moet worden beginnen met printen

--- psieve.c
+++ psieve.c
@@ -5,6 +5,7 @@
 unsigned long N = 100;
 size_t P = 2;
 
+
 unsigned long max(unsigned long a, unsigned long b){
   return (a>b) ? a : b;
 }
@@ -27,14 +28,14 @@ void sieve() {
   unsigned long remains = (N - sizes[0] + 1) % (P - 1);
 
   for (size_t index = 1; index < P; ++index)
-  {
-    sizes[index] = size;
+    {
+      sizes[index] = size;
 
-    if (index <= remains)
-      ++sizes[index];
+      if (index <= remains)
+	++sizes[index];
 
-    startsAt[index] = startsAt[index - 1] + sizes[index - 1];
-  }
+      startsAt[index] = startsAt[index - 1] + sizes[index - 1];
+    }
 
   if(core == 0)
     for(size_t p = 0; p < P; ++p)
@@ -59,7 +60,6 @@ void sieve() {
 	    prime ++;
 	  }
 
-	  
 	  // Send it to the others
 	  for (size_t proc = 1; proc < P; ++proc)
 	    bsp_put(proc, &prime, &prime, 0, sizeof(unsigned long));
@@ -67,21 +67,37 @@ void sieve() {
       bsp_sync();
 
   
-      // Otherwise, sieve the multiples
+      // Sieve all cores
       for (unsigned long multiple = prime * prime; multiple < startsAt[core] + sizes[core]; multiple += prime)
 	x[multiple - startsAt[core]] = 0;
+
       prime ++;
     }
   // Found all primes < N, so send a stopping signal
 
   unsigned long count = 0;
-
+  
   for (unsigned long index = 0; index < sizes[core]; ++index)
     if (x[index] == 1 && !(index < 2 && core==0))
       ++count;
 
-  printf("Proc %zu found %zu primes\n", core, count);
+  unsigned long counters[P];
+  bsp_push_reg(counters,P*(sizeof(unsigned long)));
+  bsp_sync();
+  int i;
+  for (i=0;i<P;i++){
+    bsp_put(i,&count,counters,core*sizeof(unsigned long),sizeof(unsigned long));
+  }
+  bsp_sync();
+  
+  if (core==0){
+    for (i=(P-1);i>0;i--)
+      counters[i-1] += counters[i];
 
+    printf("there are %zu primes",counters[0]);
+  }
+  
+  //  printf("Proc %zu found %zu primes\n", core, count);
   bsp_end();
 }
 
@@ -95,18 +111,18 @@ int main(int argc, char ** argv)
   scanf("%zu", &P);
 
   if (P > bsp_nprocs())
-  {
-    printf("Sorry, not enough processors available.\n");
-    fflush(stdout);
-    exit(1);
-  }
+    {
+      printf("Sorry, not enough processors available.\n");
+      fflush(stdout);
+      exit(1);
+    }
 
   if (P <= 1)
-  {
-    printf("Select at least two cores.\n");
-    fflush(stdout);
-    exit(1);
-  }
+    {
+      printf("Select at least two cores.\n");
+      fflush(stdout);
+      exit(1);
+    }
 
   printf("Upper bound?\n");
   fflush(stdout);
