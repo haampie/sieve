f56eb8f8245cc1fc52b0e4c0176e7a19936a7827
Harmen Stoppels <harmenstoppels@gmail.com>
2015-10-06 16:03:49 +0200

    Reworked the sieve to store half the memory and added a segmented sieve which exploits l1 cache

---
+++ segmented_sieve.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+const size_t CACHE_SIZE = 32768;
+
+void segmented_sieve(size_t limit, size_t segment_size = CACHE_SIZE)
+{
+  size_t sqrt = (int) std::sqrt((double) limit);
+  size_t count = (limit < 2) ? 0 : 1;
+  size_t s = 2;
+  size_t n = 3;
+
+  // vector used for sieving
+  std::vector<bool> sieve(segment_size);
+
+  // generate small primes <= sqrt
+  std::vector<bool> is_prime(sqrt + 1, true);
+  for (size_t i = 2; i * i <= sqrt; i++)
+    if (is_prime[i])
+      for (size_t j = i * i; j <= sqrt; j += i)
+        is_prime[j] = false;
+
+  std::vector<size_t> primes;
+  std::vector<size_t> next;
+
+  for (size_t low = 0; low <= limit; low += segment_size)
+  {
+    std::fill(sieve.begin(), sieve.end(), true);
+
+    // current segment = interval [low, high]
+    size_t high = std::min(low + segment_size - 1, limit);
+
+    // store small primes needed to cross off multiples
+    for (; s * s <= high; s++)
+    {
+      if (is_prime[s])
+      {
+        primes.push_back(s);
+        next.push_back(s * s - low);
+      }
+    }
+    // sieve the current segment
+    for (size_t i = 1; i < primes.size(); i++)
+    {
+      size_t j = next[i];
+      for (size_t k = primes[i] * 2; j < segment_size; j += k)
+        sieve[j] = false;
+      next[i] = j - segment_size;
+    }
+
+    for (; n <= high; n += 2)
+      if (sieve[n - low]) // n is a prime
+        count++;
+  }
+
+  std::cout << count << " primes found." << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+  size_t limit = 1000000000;
+  size_t size = CACHE_SIZE;
+
+  segmented_sieve(limit, size);
+  return 0;
+}
--- sieve.cpp
+++ sieve.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
 #include <string>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	size_t primes_under = 100;
+	size_t limit = 1000000000;
+	size_t halfLimit = limit / 2;
+	size_t sqrtPrimes = sqrt(limit)/2;
 	size_t counter = 0;
-	
-	if(argc == 2)
-		primes_under = stoi(argv[1]);
 
-	bool numbers[primes_under];
+	vector<bool> numbers(halfLimit, true);
 
-	for(size_t i = 0; i < primes_under; ++i)
-		numbers[i] = true;
-
-	for(size_t i = 3; i <= sqrt(primes_under); i+=2)
-		if(numbers[i])
-			for(size_t j = i*i; j < primes_under; j += i)
+	for (size_t i = 1; i <= sqrtPrimes; ++i)
+	{
+		if (numbers[i])
+		{
+			// n^2 in nieuwe index is => (n^2-1)/2 = ((4j+1)^2-1)/2 = 2(j*j+j)
+			for (size_t j = 2*(i*i+i); j < halfLimit; j += 2*i+1)
 				numbers[j] = false;
+		}
+	}
 
-	for(size_t i = 3; i < primes_under; i+=2)
-		if(numbers[i])
+	for (size_t i = 1; i < halfLimit; ++i)
+		if (numbers[i])
 			++counter;
-	
-	cout << (counter+1) << '\n';
+
+	cout << (counter + 1) << " primes <= " << limit << '\n';
 
 	return 0;
 }
