b231bc5df3dd7b345aaf1ec39ecf4c696d4821ca
Harmen Stoppels <harmenstoppels@gmail.com>
2015-10-11 11:40:12 +0200

    Use char and processors type and don't print everything

--- src/print_last.h
+++ src/print_last.h
@@ -2,7 +2,8 @@
 #define __INCLUDE_PRINTLAST__
 
 #include <vector>
+#include "../config.h"
 
-void printLast(std::vector<size_t>* truePrimes, unsigned long int P, size_t * counters, int nPrint);
+void printLast(std::vector<size_t>* truePrimes, processors P, size_t * counters, int nPrint);
 
 #endif
