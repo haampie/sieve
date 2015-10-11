2a181f179f77d9f7b062a2d989391bf30f3575b2
Harmen Stoppels <harmenstoppels@gmail.com>
2015-10-11 10:46:27 +0200

    Formatting

--- functions.h
+++ functions.h
@@ -1,46 +1,46 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
 
-int firstCross(int s, int low){
-  if (s*s > low)
-    return (s*s - low); 
-  if ((low - s*s) % (2*s) == 0)
+int firstCross(int s, int low) {
+  if (s * s > low)
+    return (s * s - low);
+  if ((low - s * s) % (2 * s) == 0)
     return 0;
-  return (2*s - ((low - s*s) % (2*s)));
+  return (2 * s - ((low - s * s) % (2 * s)));
 }
 
-void checkTwin(std::vector<size_t>* truePrimes, size_t extra_prime, unsigned long int P){
+void checkTwin(std::vector<size_t>* truePrimes, size_t extra_prime, unsigned long int P) {
   unsigned long core = bsp_pid();
-  for (unsigned long i = 0; i < P; i++){
-    if (core == i){
+  for (unsigned long i = 0; i < P; i++) {
+    if (core == i) {
       int p_size = (*truePrimes).size();
       for (int i = 0; i < p_size - 1; i++)
-	if ((*truePrimes)[i] + 2 == (*truePrimes)[i+1])
-	  std::cout << (*truePrimes)[i] << " " << (*truePrimes)[i+1] << "\n";
-      if ((*truePrimes)[p_size-1] + 2 == extra_prime)
-	std::cout << extra_prime - 2 << " " << extra_prime << "\n";
+        if ((*truePrimes)[i] + 2 == (*truePrimes)[i + 1])
+          std::cout << (*truePrimes)[i] << " " << (*truePrimes)[i + 1] << "\n";
+      if ((*truePrimes)[p_size - 1] + 2 == extra_prime)
+        std::cout << extra_prime - 2 << " " << extra_prime << "\n";
     }
     bsp_sync();
   }
 }
 
-void printLast(std::vector<size_t>* truePrimes, unsigned long int P, size_t * counters, int nPrint){
+void printLast(std::vector<size_t>* truePrimes, unsigned long int P, size_t * counters, int nPrint) {
   unsigned long start_core = 0;
   size_t skip_print = 0;
-  for (int i = P-1; i >= 0; i--){
-    if (nPrint <= counters[i]){
+  for (int i = P - 1; i >= 0; i--) {
+    if (nPrint <= counters[i]) {
       start_core = i;
       skip_print = counters[i] - nPrint;
       break;
     }
   }
-   
-  unsigned long int current_core = bsp_pid();  
-  for (unsigned long core = start_core; core < P; core ++){
+
+  unsigned long int current_core = bsp_pid();
+  for (unsigned long core = start_core; core < P; core ++) {
     bsp_sync();
-    if (current_core == core){
-      for (size_t i = skip_print; i < (*truePrimes).size(); i++){
-	std::cout << (*truePrimes)[i] << "\n";
+    if (current_core == core) {
+      for (size_t i = skip_print; i < (*truePrimes).size(); i++) {
+        std::cout << (*truePrimes)[i] << "\n";
       }
     }
     skip_print = 0;
--- src/segmented_sieve.cc
+++ src/segmented_sieve.cc
@@ -12,144 +12,119 @@ extern size_t limit;
 void segmented_sieve()
 {
   bsp_begin(P);
+  limit += 1;
   size_t sqrt = (int) std::sqrt((double) limit);
-  size_t bucketSize = CACHE_SIZE;
   size_t count = 0;
 
-  // generate small primes <= sqrt
+  // Find all the primes below sqrt by sieving all primes below sqrt(sqrt(limit))
+
   std::vector<bool> is_prime(sqrt + 1, true);
-  for (size_t i = 2; i * i <= sqrt; ++i)
+  for (size_t i = 2; i * i <= sqrt; i++)
     if (is_prime[i])
       for (size_t j = i * i; j <= sqrt; j += i)
         is_prime[j] = false;
 
   /********** End of initial sieving **********/
 
-  std::vector<size_t> sizes(P);
-  std::vector<size_t> startsAt(P);
+  std::vector<size_t> sizes(P); // This will hold the size of the vector contained on each core
+  std::vector<size_t> startsAt(P); // Starting values of the range of each core
+
   sizes[0] = std::max(sqrt, limit / P);
 
-  size_t approxSize = (limit - sizes[0]) / (P - 1);
-  size_t rem = (limit - sizes[0]) % approxSize;
+  if (P > 1) {
+    size_t approxSize = (limit - sizes[0]) / (P - 1);
+    size_t rem = (limit - sizes[0]) % approxSize;
 
-  for (processors core = 1; core < P; ++core)
-    sizes[core] = approxSize;
+    for (int core = 1; core < P; core++)
+      sizes[core] = approxSize;
 
-  for (size_t core = 1; core < rem; ++core)
-    ++sizes[core];
+    for (int core = 1; core < rem; core++)
+      sizes[core]++;
 
-  startsAt[0] = 0;
+    startsAt[0] = 0;
+    for (int i = 1; i < P; i++)
+      startsAt[i] = startsAt[i - 1] + sizes[i - 1];
+  }
+  unsigned long core = bsp_pid(); // core id
 
-  for (processors i = 1; i < P; ++i)
-    startsAt[i] = startsAt[i - 1] + sizes[i - 1];
-
-  size_t core = bsp_pid();
-
-  std::vector<size_t> primes;
-  std::vector<size_t> next;
-  std::vector<size_t> truePrimes;
+  std::vector<size_t> primes; // Will hold primes that need to be sieved for each bucket
+  std::vector<size_t> next; // Next[i] contains the local index where we first need to start sieving primes[i]
+  std::vector<size_t> truePrimes; // Will contain the actual prime numbers
 
   size_t start = startsAt[core];
   size_t n = start - (start % 2) + 1;
 
-  size_t next_pos;
   size_t end_core = startsAt[core] + sizes[core];
   size_t s = 2;
 
-  /**
-   * isPrime tells whether the ith element in the bucket is prime
-   * We use char since it is only one byte long
-   */
-  std::vector<char> isPrime(std::min(bucketSize, sizes[core]));
+  std::vector<bool> local_sieve(std::min(size, sizes[core]));
 
-  // Start sieving bucket by bucket
-  // TODO: move this to a seperate function.
-  for (size_t low = start; low < end_core; low += bucketSize)
+  int bucket_size = CACHE_SIZE; // how big is the interval that we sieve over each time
+  int num_primes = 0;
+
+  for (size_t low = start; low < end_core; low += size)
   {
-    std::fill(isPrime.begin(), isPrime.end(), 1);
 
-    // current segment = interval [low, high]
-    size_t high = std::min(low + bucketSize - 1, end_core);
+    size_t high = std::min(low + size - 1, end_core);
+    std::fill(local_sieve.begin(), local_sieve.end(), true);
 
-    // store small primes needed to cross off multiples
-    for (; s * s <= high; ++s)
+    bucket_size = high - low;
+
+    while (s * s <= high)
     {
       if (is_prime[s])
       {
+        num_primes++;
         primes.push_back(s);
-        next_pos = 0;
-        if (s * s < low)
-        {
-          next_pos = (ceil(low / (float)s) - s);
-
-          if (next_pos % 2 == 1)
-            next_pos += 1;
-
-          next_pos = s * next_pos;
-        }
-        next.push_back( s * s + next_pos - low);
+        next.push_back(firstCross(s, low));
       }
+      s++;
     }
 
-    for (size_t i = 1; i < primes.size(); ++i)
+    for (int t = 0; t < num_primes; t++)
     {
-      size_t j = next[i];
+      int j = next[t];
+      
+      for (int k = 2 * primes[t]; j <= bucket_size; j += k )
+        local_sieve[j] = false;
 
-      for (size_t k = 2 * primes[i] ; j < std::min(bucketSize, high - low); j += k)
-        isPrime[j] = 0;
-
-      next[i] = j - bucketSize;
+      next[t] = j - bucket_size - 1;
     }
 
     for (; n <= high; n += 2)
-    {
-      if (isPrime[n - low])
-      {
-        ++count;
+      if (local_sieve[n - low]) {
+        count++;
         truePrimes.push_back(n);
       }
-    }
   }
+
+  /********** Sieving done! Now the counters need to be added **********/
 
   if (core == 0)
-    ++truePrimes[0];
+    truePrimes[0]++;
 
-  /********** A sanity check print to screen *************/
-
-  for (int i = 0; i < P; ++i)
-  {
-    bsp_sync();
-    if (bsp_pid() == i)
-    {
-      // std::cout << "\n";
-      // for (size_t i = 0; i < truePrimes.size(); i++) {
-      //   std::cout << truePrimes[i] << "\n";
-      // }
-      std::cout << count << " primes found." << "\n";
-      bsp_sync();
-    }
-  }
-
-  size_t counters[P];
+  size_t extra_prime; // will hold the first prime of the next core for checking the twin primes
+  size_t counters[P]; // will hold the counters of all cores
+  bsp_push_reg(&extra_prime, sizeof(size_t)); // register them
   bsp_push_reg(&counters, P * sizeof(size_t));
   bsp_sync();
 
-  for (int i = 0; i < P; ++i)
-    bsp_put(i, &count, &counters, i * sizeof(size_t), sizeof(size_t));
-
+  bsp_put((core - 1) % P, &(truePrimes[0]), &extra_prime, 0, sizeof(size_t));
   bsp_sync();
 
-  if (core == 1)
-  {
-    for (int i = 0; i < P ; ++i)
-    {
-      //std::cout << counters[i] << "\t";
+  // checkTwin(&truePrimes, extra_prime, P);
 
-      for (int i = P - 1; i > 0; i--)
-        counters[i - 1] += counters[i];
-    }
-  }
+  for (int i = 0; i < P; i++)
+    bsp_put(i, &count, &counters, core * sizeof(size_t), sizeof(size_t));
+  bsp_sync();
 
-  //  std::cout << counters[0] << "\n";
+  for (int i = P - 1; i > 0; i--)
+    counters[i - 1] += counters[i];
+
+  printLast(&truePrimes, P, counters, nPrint);
+
+  if (core == 0)
+    std::cout << counters[0] << " primes.\n";
+
   bsp_end();
 }
