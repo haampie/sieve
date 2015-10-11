29d670dd377a95ed1081b4d1e90b483166ae46f8
Harmen Stoppels <harmenstoppels@gmail.com>
2015-10-11 11:33:08 +0200

    Make limit, cores and nprint variable

--- main.cc
+++ main.cc
@@ -4,13 +4,25 @@
 #include "src/parallel.h"
 
 #include <iostream>
+#include <string>
 
 processors P = 4;
 size_t limit = 1000000000;
 size_t nPrint = 100;
 
+using namespace std;
+
 int main(int argc, char ** argv)
 {
+  if(argc > 1)
+    limit = stoul(argv[1]);
+
+  if(argc > 2)
+    P = stoul(argv[2]);
+
+  if(argc > 3)
+    nPrint = stoul(argv[3]);
+
   bsp_init(segmented_sieve, argc, argv);
   segmented_sieve();
 }
