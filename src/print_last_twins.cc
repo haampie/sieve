01876aaa7ea2c4395e0a129117c2812ff3af74d8
Harmen Stoppels <harmenstoppels@gmail.com>
2015-10-12 15:04:42 +0200

    Print the last twins in correct order

---
+++ src/print_last_twins.cc
@@ -0,0 +1,42 @@
+#include "print_last.h"
+
+#include <iostream>
+#include "parallel.h"
+
+using namespace std;
+
+void printLastTwins(vector<size_t> *twins, processors P, size_t *counters, int nPrint)
+{
+  // Determine in what core we need to starr printing and what the local index is of
+  // the first prime that needs to be printed.
+
+  processors startCore = 0;
+  size_t skipPrint = 0;
+
+  if (counters[0] < nPrint) { // We need to print more than we have
+    cout << "2\n"; //
+  } else if (counters[0] != nPrint) { // if counters[0] == nPrint, we simply need to print all found primes
+    for (int i = P - 1; i >= 0; --i)
+    {
+      if (nPrint <= counters[i]) // There are more that nPrint primes contained in cores i,..,P-1.
+      {
+        startCore = i;
+        skipPrint = counters[i] - nPrint; // How many primes in core i may be skipped
+        break;
+      }
+    }
+  }
+
+  processors current_core = bsp_pid();
+  for (processors core = startCore; core < P; ++core)
+  {
+    bsp_sync();
+
+    if (current_core == core) // make sure the cores only print one at a time
+      for (size_t i = skipPrint; i < twins->size(); ++i)
+        cout << (*twins)[i] - 2 << ' ' << (*twins)[i] << '\n';
+
+    skipPrint = 0;
+  }
+  bsp_sync();
+}
---
+++ src/print_last_twins.h
@@ -0,0 +1,10 @@
+#ifndef __INCLUDE_PRINTLASTTWINS__
+#define __INCLUDE_PRINTLASTTWINS__
+
+#include <vector>
+#include <stdlib.h>
+#include "../config.h"
+
+void printLastTwins(std::vector<size_t>* twins, processors P, size_t * counters, int nPrint);
+
+#endif
